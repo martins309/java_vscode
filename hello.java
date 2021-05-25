e104dc02a3ed599dd3d166c0251b3138e72ffec1
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-24 22:47:59 -0400

    creating classes in java

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("alloe gov'na");
+        int myFirstNumber = (10 + 5) + (2 * 10) ;
+        int mySecondNumber = 12;
+        System.out.println(myFirstNumber);
+    }
+}
