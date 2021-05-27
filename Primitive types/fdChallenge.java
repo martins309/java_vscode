330b96e36493d72650d307d3641b3a4c9734f4a1
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-27 17:47:16 -0400

    conversion challenge

---
+++ Primitive types/fdChallenge.java
@@ -0,0 +1,9 @@
+//convert a given number in pounds to kilograms 
+//create a variable with the appropriate type to store tje number of pinds to ne converted to kilograms
+//Calculate the result i.e. the number of kilograms based on the contents of the above variable
+// and the store the result in a seconds appropriate variable
+//print out the result
+
+ouble poundConversion = .453592;
+        double kilograms = poundConversion;
+        System.out.println("This is the value of the conversion = " + 100 * kilograms + "kg");
