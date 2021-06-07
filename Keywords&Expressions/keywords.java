4faf03c3ad0fc00dd76103ce43dbe3b293b9d288
Andrew Martins <andrew_omartins@yahoo.com>
2021-06-07 12:56:46 -0400

    keywords and expressions

---
+++ Keywords&Expressions/keywords.java
@@ -0,0 +1,21 @@
+{
+
+    public static void main(String[] args) {
+	// a mile is equal to 1.609344 kilometers
+        //expression components are variables values and operators
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        System.out.println(kilometers);
+        if(highScore == 50) {
+            System.out.println("THis is an expression");
+            // in the following code that I will type below, write down what parts of the code
+            //are expressions
+            int score = 100;
+            if(score > 90) {
+                System.out.println("You got the high score");
+                score = 0;
+        }
+
+        }
+    }
+}
