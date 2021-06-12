8b399e8af63ff725f29622a4e184627260a7eea1
Andrew Martins <andrew_omartins@yahoo.com>
2021-06-11 23:52:09 -0400

    comments

--- Challenges/SpeedConverter.java
+++ Challenges/SpeedConverter.java
@@ -1,3 +1,15 @@
+//write a method called toMilesPerHour that has 1 parameter of type double with the name kilometersPerHour
+//This method needs to return the rounded value of the calculation of type long
+// if the parameter kilometersPerHour is less than 0 then the method tMPH needs to return -1 to indicate invalid value
+//Else if it positive calculate the value of miles per hour, round it and return it
+
+//Write another method called printConversion with 1 parameter of type double with the name kilometersPerHour
+//THis method should not return anuthing(void) and it needs to calculate milesPerHour from the kilometersPerHour parameter
+//it needs to print a message in the format "XX km/h = YY mi/h"
+// XX = kPH
+//YY = rounded mPH from kPH parameter
+
+
 public class SpeedConverter {
     public static void main(String[] args) {
         public static long toMilesPerHour(double kilometersPerHour) {
