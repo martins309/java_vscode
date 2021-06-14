d5aef28b9b0e04b4aacfc5d6621fdc38760cb098
Andrew Martins <andrew_omartins@yahoo.com>
2021-06-14 17:44:38 -0400

    leap year and method call

---
+++ Challenges/leapYear/leapYearMC.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+     boolean calenderYear = LeapYear.isLeapYear(1800);
+        System.out.println("Is this year a leap year? " + calenderYear);
+    }
+}
